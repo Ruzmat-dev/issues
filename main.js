68cac5920e0f97ea7d67db60c28ec0855f3a11e8
Universal0090 <idealxorezm@gmail.com>
2024-01-01 23:30:09 +0500

    some change issuse

---
+++ main.js
@@ -0,0 +1,91 @@
+//1
+// let a = prompt('')
+// if(a  > 10) {
+//     console.log(a+3)
+// } else if ( a < 10) {
+//     console.log(2*a)
+// } else {
+//     console.log(22);
+// }
+
+// console.log(a  > 10 ? a+3 : a < 10 ? 2 * x : 22);
+
+// 2
+// let a = prompt('')
+// let b = Math.floor(a / 3)
+// console.log(b);
+
+// 3
+// let a = prompt('')
+// let b = Math.floor(a % 3)
+// console.log(b);
+
+// 4
+// let a = prompt()
+// a = parseInt(a)
+// if(a == Number) {
+//     if(a % 2 == 0) {
+//         console.log("juft");
+//     } else {
+//         console.log("toq");
+//     }
+// }else {
+//     console.log('apgni chqir');
+// }
+
+// 5
+// a soni berilgan.  0 dan a gacha bo'lgan sonlaryig'indisini toping  va yig'indini konsolga chiqaring. a sonini ham hisobga oling.
+
+// a soni berilgan boladi biz missol qilib 10 ni olamiz
+// const a = 10
+// biz bitta ozgaruvchi ochamiz va uni 0 ga teglab qoyib shuni ustiga qoshib boramiz
+// let sum = 0
+// JS for method dan foydalanib har bir soni aylanib chiqamiz
+// for(i=1; a >= i; i++){
+//     sum = sum + i
+// }
+// for ishlab bolgach natija chiqadi bizda natija 55 chiqdi
+// console.log(sum);
+
+// a soni berilgan boladi biz missol qilib 10 ni olamiz
+// const a = 10;
+// formulaga tashaymiz S = (a*(a+1))/2
+// let sum = ((a * (a + 1)) / 2);
+// bizda natija 55 chiqadi
+// console.log(sum);
+
+// 6
+// n soni berilgan.
+// Quyidagi yig'indini hisoblovchi dastur yozing.
+// (1 + 1/2 + 1/3 + 1/4 ..... 1/n ).
+// let n = 5
+// let sum = 0
+// for(let i = 1; n >=i; i++) {
+//     sum += 1/i
+// }
+// console.log(sum);
+
+// 7
+// Misol Sharti
+// n soni berilgan.
+// Geometrik shaklni ekranga chiqaring.
+// let n = 9
+// for(let i = 0; n >= i; i++){
+//     let line = ''
+//     for(let e = 0; e <=i; e++){
+//         line += '*'
+//     }
+//     console.log(line);
+// }
+
+// 8
+
+// let n = 10;
+
+// for (let i = 0; i < n ; i++) {
+//   let line = '';
+//   for (let j = 0; j < n; j++) {
+//     line += `(${i},${j})`;
+//   }
+//   console.log(line);
+// }
